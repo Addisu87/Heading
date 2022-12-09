e846e3c8ee9616f0b21e148b7f2124eafc037fa2
Addisu <addisuhaile87@gmail.com>
2022-12-09 23:47:43 +0300

    Add a style for headings

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <header className="text-center font-bold text-4xl">
+      <header className="text-center font-bold text-4xl m-6">
         <h1 className="App-title">Front end developer test project</h1>
       </header>
       <Home />
