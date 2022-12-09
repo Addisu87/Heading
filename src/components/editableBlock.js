9286fc3fc68acf0923a5a6039bab982801bce578
Addisu <addisuhaile87@gmail.com>
2022-12-09 20:37:19 +0300

    Update the typo error

--- src/components/editableBlock.js
+++ src/components/editableBlock.js
@@ -115,7 +115,7 @@ class EditableBlock extends React.Component {
         )}
 
         <ContentEditable
-          className="Block"
+          className="border border-x-2 outline-dashed w-80 items-center justify-center p-2"
           innerRef={this.contentEditable}
           html={this.state.html}
           tagName={this.state.tag}
--- src/components/editablePage.js
+++ src/components/editablePage.js
@@ -53,7 +53,7 @@ class EditablePage extends React.Component {
 
   render() {
     return (
-      <div className="bg-gray-800">
+      <div className="bg-gray-400/20">
         {this.state.blocks.map((block, key) => {
           return (
             <EditableBlock
