d6fc8316b8f318ef35de140fd3609bea47f7017b
Addisu <addisuhaile87@gmail.com>
2022-12-10 00:17:46 +0300

    Update the header

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-      <header className="text-center font-bold text-4xl m-6">
-        <h1 className="App-title">Front end developer test project</h1>
+      <header className="text-center font-bold text-4xl m-6 bg-slate-300/20 shadow-slate-400 h-12 p-2">
+        <h1 className="">Front end developer test project</h1>
       </header>
       <Home />
     </div>
